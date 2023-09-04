8fc7ca7e63568098380474d3feebfcc206c68cb2
Igor Maljuk <tigormal@yandex.ru>
2023-09-04 04:19:10 +0300

    Refreshed install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+pip install -r requirements.txt
 cp -i ./src/main.py /bin/remotegamepad.py
 # crontab -l > .tmpcron
+echo uinput | sudo tee -a /etc/modules
 crontab -l | { cat; echo "@reboot python /bin/remotegamepad.py &"; } | crontab -
 # echo "@reboot python /bin/remotegamepad.py &" >> .tmpcron
 # crontab .tmpcron
